19e64cf4ce43a2ae550b32f3da42284ee1133bc3
oltapllana <oltapllana@gmail.com>
2024-10-04 00:12:51 +0200

    first commit

--- src/Components/Quotes/Quotes.jsx
+++ src/Components/Quotes/Quotes.jsx
@@ -1,32 +1,64 @@
-import {useState} from "react"
+import {useState , useRef} from "react"
 import React from 'react'
 import "./Quotes.css"
-
+import { categories } from "./categories"
 
 const Quotes = () => {
 
 
-  const [quoteData, setQuoteData] = useState("") 
-  
+  const [quoteData, setQuoteData] = useState([]) 
+    const selectValue = useRef(null)
+  const apiKey = import.meta.env.VITE_API_KEY
   async function generateQuote(){
-    const url = `https://supernatural-api.onrender.com/quotes/random`;
-    const response = await fetch(url);
-    const data = await response.json();
     
-    setQuoteData(data)
+    try {
+    const url = `https://famous-quotes4.p.rapidapi.com/random?category=${selectValue.current.value}&count=1`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': apiKey,
+        'x-rapidapi-host': 'famous-quotes4.p.rapidapi.com'
+      }
+    };
+    
+      const response = await fetch(url, options);
+      const result = await response.json();
+      console.log(result);
+      setQuoteData(result)
+    } 
+    catch (error) {
+      console.error(error);
+    }
+    
   }
+ 
  
 
 
   return (
     <div className="container">
-    <h1>SuperNatural Quotes</h1>
+    <h1>Quotes</h1>
+    <p>100+ categories to choose from</p>
+    <p>Select a category: </p>
+    <select ref={selectValue} >
+    <option  value="architecture">Architecture</option>
+    {
+      categories.map((category, index) => (
+        <option key={index} value={category}>{category.slice(0,1).toUpperCase() + category.slice(1,category.length).toLowerCase()}</option>
+      ))
+    }
+    </select>
     <div className="quote-container">
-      <h2>The Quote</h2>
-   {quoteData ? <>
-    <p>{quoteData.line_0.quote}</p>
-    <p>-{quoteData.line_0.character.name}-</p>
-                </> : <></>}
+      {quoteData[0] ? <></> :  <h2>The Quote</h2>}
+     
+    {quoteData[0] &&
+    <>
+      <h2>Category: {quoteData[0].category.slice(0,1).toUpperCase() + quoteData[0].category.slice(1, quoteData[0].category.length).toLowerCase()}</h2>
+      <p>{quoteData[0].text}</p>
+      <p>-{quoteData[0].author}-</p>
+    </>
+    
+    }
 
       <button onClick={generateQuote}>Generate Quote</button>
     </div>
